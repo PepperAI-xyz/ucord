288f58ce55d7d646a6cfad88a6401208ce312178
Jeff <99515656+Jeff53978@users.noreply.github.com>
2023-02-05 03:09:22 +0100

    Create status.py

---
+++ status.py
@@ -0,0 +1,11 @@
+class Status:
+    dnd = "dnd"
+    idle = "idle"
+    online = "online"
+    offline = "offline"
+
+class ActivityType:
+    game = 0
+    streaming = 1
+    listening = 2
+    watching = 3
