a4933376c64816c415bff21b61800c0badcfa053
Jeff <99515656+Jeff53978@users.noreply.github.com>
2023-02-05 03:28:25 +0100

    Create simple_bot.py

---
+++ tests/simple_bot.py
@@ -0,0 +1,14 @@
+import ucord
+
+client = ucord.Client()
+client.set_token("token")
+client.run()
+
+@client.on_ready
+def on_ready(client):
+    print(f"Logged in as {client.user}")
+
+@client.slash_command(name="ping", description="Ping command")
+def ping(ctx):
+    embed = ucord.Embed(title="Pong!", description="Pong!")
+    ctx.reply("Bot is online", embed=embed)
